6e6fd934061233080121571a3c29b73f0ba07fd9
Reid Mayfield <rmayf@arista.com>
2018-12-28 11:25:56 -0800

    crawler can take a --interval option to make it run in a loop

--- crawler.php
+++ crawler.php
@@ -5,53 +5,16 @@ require_once 'emailerPrivateInfo.php';
 // don't allow it to be run from web
 if (isset($_SERVER['REMOTE_ADDR'])) die('Permission denied.');
 
-chdir( '/var/www/html/' );
-$punchcard = '.punchcard';
-// check last runtime by getting mtime of punchcard file
-$prevTime = filemtime( $punchcard );
-// update mtime by touching punchcard
-touch( $punchcard );
-echo( "Previous run " . date( "F d Y H:i:s.", $prevTime ) ."\n" );
+$seperator = "/";
+$pathComponents = explode( $seperator, __FILE__ );
+$workDir = implode( $seperator, array_slice( $pathComponents, 0, count( $pathComponents ) - 1 ) );
+chdir( $workDir );
 
-// crawl the tree, checking all folders to see if they are new
-$newAlbums = array();
-handleDir( 'Home/' );
-if( !empty( $newAlbums ) ) {
-   echo( "Found new albums!\n" );
-   print_r( $newAlbums );
-   $manager = new MongoDB\Driver\Manager();
-
-   require_once 'swiftmailer/lib/swift_required.php';
-   $transport = Swift_SmtpTransport::newInstance('smtp.gmail.com', 465, "ssl")
-     ->setUsername( $emailUsername )
-     ->setPassword( $emailPassword );
-   
-   $mailer = Swift_Mailer::newInstance($transport);
-   $prefix = 'http://beanpictures.homeftp.net/sendNotification.php?path=';
-   $msg = file_get_contents( 'newAlbum.template' );
-   foreach( $newAlbums as $album ) {
-      $ar = explode( '/', $album );
-      $title = end( $ar );
-      $key = sha1(microtime(true).mt_rand(10000,90000));
-      $msg .= "<a href=\"$prefix$album&key=$key\">$title</a><br>";
-
-      $command = new MongoDB\Driver\Command( array( "update" => "albums",
-                       "updates" => [ array( "q" => array( "path" => $album ),
-                                             "u" => array( "path" => $album,
-							   "key" => $key,
-							   "notified" => false ),
-					     "upsert" => true ) ] ) );
-      $manager->executeCommand( 'main', $command );
-   }
-   $msg .= "</body></html>";
-   $message = Swift_Message::newInstance('Album Notification Confirmation')
-     ->setFrom(array( $emailUsername => 'Album Crawler'))
-     ->setTo(array('brockband1@gmail.com'))
-     ->setCC(array('rmayf3@gmail.com' ) )
-     ->setBody( $msg, 'text/html' );
-   
-   $result = $mailer->send($message);
+$options = getopt( "", array( "interval::" ) );
+if( isset( $options[ 'interval' ] ) && empty( $options[ 'interval' ] ) ) {
+   die( "--interval requires an argument in minutes (e.g. --interval=45)" );
 }
+$delaySeconds = $options[ 'interval' ] * 60;
 
 function handleDir( $dirString ) {
    global $prevTime, $newAlbums;
@@ -66,9 +29,51 @@ function handleDir( $dirString ) {
          $ctime = filemtime( $dirString . $f );
          if( $isLeaf && $ctime > $prevTime ) {
             array_push( $newAlbums, $dirString . $f );
-	    $leaf = false;
+            $leaf = false;
          }
       }
    }
    return $leaf;
 }
+
+do {
+   // crawl the tree, checking all folders to see if they are new
+   $newAlbums = array();
+   handleDir( 'Home/' );
+   if( !empty( $newAlbums ) ) {
+      echo( "Found new albums!\n" );
+      print_r( $newAlbums );
+      $manager = new MongoDB\Driver\Manager();
+
+      require_once 'swiftmailer/lib/swift_required.php';
+      $transport = Swift_SmtpTransport::newInstance('smtp.gmail.com', 465, "ssl")
+        ->setUsername( $emailUsername )
+        ->setPassword( $emailPassword );
+
+      $mailer = Swift_Mailer::newInstance($transport);
+      $prefix = 'http://beanpictures.homeftp.net/sendNotification.php?path=';
+      $msg = file_get_contents( 'newAlbum.template' );
+      foreach( $newAlbums as $album ) {
+         $ar = explode( '/', $album );
+         $title = end( $ar );
+         $key = sha1(microtime(true).mt_rand(10000,90000));
+         $msg .= "<a href=\"$prefix$album&key=$key\">$title</a><br>";
+
+         $command = new MongoDB\Driver\Command( array( "update" => "albums",
+                          "updates" => [ array( "q" => array( "path" => $album ),
+                                                "u" => array( "path" => $album,
+                                                              "key" => $key,
+                                                              "notified" => false ),
+                                                "upsert" => true ) ] ) );
+         $manager->executeCommand( 'main', $command );
+      }
+      $msg .= "</body></html>";
+      $message = Swift_Message::newInstance('Album Notification Confirmation')
+        ->setFrom(array( $emailUsername => 'Album Crawler'))
+        ->setTo(array('brockband1@gmail.com'))
+        ->setCC(array('rmayf3@gmail.com' ) )
+        ->setBody( $msg, 'text/html' );
+
+      $result = $mailer->send($message);
+   }
+} while( $delaySeconds && sleep( $delaySeconds ) );
